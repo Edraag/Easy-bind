0bb30fb10da038e50dae4616b000292499f74b8d
Marius Gaarde <marius@Mariuss-Mac-mini.local>
2018-12-23 13:59:10 +0100

    Added example-code.lisp

---
+++ example-code.lisp
@@ -0,0 +1,52 @@
+
+;;; ---------- EASY-BIND - Easy local binding for Common Lisp ----------
+;;
+;; Version 1.0
+;; Copyright (C) Marius Gaarde 2018. All rights reserved. Licensed under the MIT license.
+;;
+;; THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+;; INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+;; AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+;; DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+;;
+;;
+
+;; A simple infix reader showing the use of destructuring in a with form
+
+(defparameter *previous-readtables* nil)
+(defparameter *infix-syntax*        nil)
+
+(defun read-infix (input-stream char)
+  (declare (ignore char))
+  (with list = (read-delimited-list #\] input-stream t)
+	(&optional arg1 operator arg2 &rest rest) = list
+	(cond ((null list)     ())
+	      ((null operator) arg1)
+	      ((null arg2)     (list operator arg1))
+	      ((null rest)     (list operator arg1 arg2))
+	      (t
+	       (with rest-operands being
+		     (loop
+			for pair on (cdr list) by #'cddr
+			unless (cdr pair) do
+			  (error "Malformed infix expression - operand must follow operator")
+			unless (eq operator (car pair)) do
+			  (error "Malformed infix expression - only one operator allowed")
+			collect (cadr pair))
+		     (list* operator arg1 rest-operands))))))
+
+(defun enable-infix ()
+  "Enable infix expressions like [1 + 2 + 3]."
+  (unless *infix-syntax*
+    (push *readtable* *previous-readtables*)
+    (setq *readtable* (copy-readtable))
+    (set-macro-character #\[ 'read-infix)
+    (set-syntax-from-char #\] #\))
+    (setq *infix-syntax* t)))
+
+(defun disable-infix ()
+  (when *infix-syntax*
+    (setq *readtable* (pop *previous-readtables*))
+    (setq *infix-syntax* nil)))
+
